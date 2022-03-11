624dc7269b2c6264e32ed02b4dc52729d58648bc
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-11 12:18:21 +0530

    first

---
+++ components/Expenseitems.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <h2>Expense Item!</h2>
+        <div> Food Rs 10 </div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    );
+  }
+  export default ExpenseItem;
+  
