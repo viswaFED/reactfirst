590d532e584dc0ca2ed2055067cb01613a56182c
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-13 15:30:40 +0530

    ExpenseDetails

---
+++ components/ExpenseDetails.js
@@ -0,0 +1,16 @@
+function ExpenseDetails(props) {
+  const amount = props.amount;
+/*const location = props.locatio}
+  const title = props.title}*/
+
+  return (
+    <div className="expense-description">
+      {/*<div>{title}</div>*/}
+      {/*<div>{location}</div>*/}
+      <div className="expense-item__price">{amount}</div>
+    </div>
+  );
+}
+
+
+export default ExpenseDetails;
--- components/Expenseitems.js
+++ components/Expenseitems.js
@@ -1,14 +1,21 @@
 import "./Expenseitems.css";
+import ExpenseDetails from "./ExpenseDetails";
+import ExpenseDate from "./ExpenseDate.js";
 import React from "react";
 
 function ExpenseItem(props) {
-  return<div className="expense-item">
-  <div>{props.date.toISOString()}</div>
-  <div>{props.location}</div>
-  <div className="expense-item__description">
-    <h2>{props.title}</h2>
-    <div className="expense-item__price"> {props.amount}</div>
-  </div>
-  </div>
+  return (
+
+		<div className='expense-item'>
+
+			<ExpenseDate date ={props.date} />
+      
+			<ExpenseDetails amount={props.amount} />
+      
+
+		</div>
+    
+
+)
 }
 export default ExpenseItem;
