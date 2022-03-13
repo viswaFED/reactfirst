1451998b88ddebcb28b35c58e81d9bda1720bfcd
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-03-13 15:40:39 +0530

    addedDateDetails

--- components/Expenseitems.js
+++ components/Expenseitems.js
@@ -1,21 +1,19 @@
 import "./Expenseitems.css";
-import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate.js";
 import React from "react";
 
 function ExpenseItem(props) {
   return (
-
-		<div className='expense-item'>
-
-			<ExpenseDate date ={props.date} />
-      
-			<ExpenseDetails amount={props.amount} />
-      
-
-		</div>
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      {/*<div>{props.location}</div>*/}
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price"> {props.amount}</div>
+      </div>
+     
+      </div>
     
-
-)
+  );
 }
 export default ExpenseItem;
